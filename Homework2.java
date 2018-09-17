c76bfe89775839b9e68b002ab2a9f80941610133
Reece Boulware <boulwree@berkeleyprep.org>
2018-09-17 14:11:24 -0400

    hello

--- Homework2.java
+++ Homework2.java
@@ -1,18 +1,24 @@
 /*
- * First and Last Names
+ * Reece Boulware
  */
 
 import java.awt.Rectangle;
 
 public class Homework2 {
+	private String c;
+	public Homework2(String input) {
+    c = input;
+	}
 
-	/* Write the constructor for Homework2.
-	 * It takes in one parameter: a String
-	 * Save the string as a private instance variable
-	 */
+	 // Write the constructor for Homework2.
+	 //It takes in one parameter: a String
+	 //Save the string as a private instance variable
 
-
-	/* Write the method upperase
+public String uppercase() {
+	 c = c.toUpperCase();
+	return c;
+}
+	/* Write the method uppercase
 	 * It does not take in any parameters
 	 * It returns a String
 	 * Use String methods to turn the instance variable String into all
@@ -20,7 +26,13 @@ public class Homework2 {
 	 * Hint: Search the String class for 'uppercase'
 	 */
 
-
+  public String strip(String strip){
+		strip = strip.replace(" ","");
+		strip = strip.replace(",", "");
+		strip = strip.replace(".", "");
+		strip = strip.replace("!", "");
+		return strip;
+	}
 	/* Write the method strip
 	 * It takes in one paramter: a String
 	 * It returns a String
@@ -33,12 +45,12 @@ public class Homework2 {
 	/* Fix the errors in the method problem4
 	 */
 	public Rectangle problems() {
-		Rectangle r1 = (5, 10, 15, 20);
-		double width = Rectangle(5, 10, 15, 20).getWidth();
-		Rectangle r2;
+		Rectangle r1 = new Rectangle (5, 10, 15, 20);
+		double width = new Rectangle(5, 10, 15, 20).getWidth();
+		Rectangle r2 = new Rectangle();
 		r2.translate(15, 25);
-		r3 = new Rectangle();
-		r3.translate("Far, far away");
+		Rectangle r3 = new Rectangle();
+		r3.translate( 22, 36);
 		return r3;
 	}
 
@@ -47,6 +59,11 @@ public class Homework2 {
 	 * parameter Rectangle
 	 */
 	public double getPerimeter(Rectangle rect) {
+		double rectWidth = rect.getWidth();
+		double rectLength = rect.getHeight();
+		double rectPeri = 2 * rectWidth + 2 * rectLength;
+		return rectPeri;
+
 
 	}
 
